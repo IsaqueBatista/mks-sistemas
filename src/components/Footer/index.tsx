dcb78b40599198162ac91a9271f963b8e5963d6f
IsaqueBatista <isaquebatista1716@gmail.com>
2023-11-14 22:19:23 -0300

    Footer Finished

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import * as S from "./styles";
+
+export default function Footer() {
+  return (
+    <>
+      <S.P>MKS sistemas © Todos os direitos reservados</S.P>
+    </>
+  );
+}
---
+++ src/components/Footer/styles.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const P = styled.p`
+  width: 100%;
+  height: 34px;
+  background: #eee;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  color: #000;
+
+  font-size: 12px;
+  font-weight: 400;
+  line-height: normal;
+`;
